871ae00261eeb675fb36414f2b8be717845ad31b
Jay Ramirez <jay.ramirez@nuworks.ph>
2014-02-03 15:59:15 +0800

    hahaha

---
+++ page.php
@@ -0,0 +1,8 @@
+<div style="width:400px; height:400px;background: #ccc; margin: 0 auto; padding: 20px; position:relative; border: dashed 2px #900; font:12px arial">
+	<div class="lytebox-close">x</div>
+
+	<p>This is a popup with separate html or php file.</p>
+
+	<p>Here is your data: <?php echo $_GET['str']?></p>
+	
+</div>
